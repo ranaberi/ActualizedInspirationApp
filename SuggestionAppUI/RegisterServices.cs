6de67cb38a229653f842cf75703b3c7fb2d4980f
Gebruiker <Gebruiker@RANA>
2024-01-29 09:39:20 +0100

    Add project files.

---
+++ SuggestionAppUI/RegisterServices.cs
@@ -0,0 +1,12 @@
+﻿namespace SuggestionAppUI;
+
+public static class RegisterServices
+{
+    public static void ConfigureServices(this WebApplicationBuilder builder)
+    {
+        // Add services to the container.
+        builder.Services.AddRazorComponents()
+            .AddInteractiveServerComponents();
+        builder.Services.AddMemoryCache();
+    }
+}
