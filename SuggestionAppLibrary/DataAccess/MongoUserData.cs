feb62f9823fcd7d21de78a211cbf22d69f63b04b
Rana <ranaberi25@gmail.com>
2024-01-30 23:59:26 +0100

    user

---
+++ SuggestionAppLibrary/DataAccess/MongoUserData.cs
@@ -0,0 +1,14 @@
+﻿
+using MongoDB.Driver;
+using System.Data;
+
+namespace SuggestionAppLibrary.DataAccess;
+
+public class MongoUserData
+{
+    private readonly IMongoCollection<UserModel> _users;
+    public MongoUserData(IDbConnection db)
+    {
+        
+    }
+}
