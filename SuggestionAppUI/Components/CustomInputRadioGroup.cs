ffb6fa3c051caaf7c976bc583db1ab982dce02ec
Rana <ranaberi25@gmail.com>
2024-02-04 13:22:15 +0100

    create suggestion page. custom input component

---
+++ SuggestionAppUI/Components/CustomInputRadioGroup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+namespace SuggestionAppUI.Components;
+
+public class CustomInputRadioGroup<TValue>: InputRadioGroup<TValue>
+{
+    private string _name;
+    private string _fieldClass;
+
+    /// <summary>
+    /// Get the field identifier or puts an empty string if none exists
+    /// </summary>
+    protected override void OnParametersSet()
+    {
+        var fieldClass = EditContext?.FieldCssClass(FieldIdentifier) ?? string.Empty;
+        if(fieldClass != _fieldClass || Name != _name)
+        {
+            _fieldClass = fieldClass;
+            _name = Name;
+            base.OnParametersSet();
+        }
+    }
+}
